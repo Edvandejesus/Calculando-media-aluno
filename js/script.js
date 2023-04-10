e77f8a7964c057c97600e3f25e2763fa27b654c3
Edvan de Jesus <edvanbolsavalores@gmail.com>
2023-04-10 00:26:00 -0300

    projeto finalizado

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const frm = document.querySelector("form")
+
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nome = frm.inNome.value
+
+    const nota1 = Number(frm.inNota1.value)
+    const nota2 = Number(frm.inNota2.value)
+    const media = (nota1 + nota2) / 2
+
+    resp1.innerText = `A sua méida é : ${media.toFixed(2)}`
+
+
+    if (media >= 7) {
+        resp2.innerText = `Parabens ${nome} ! Você Foi Aprovado  `
+        resp2.style.color = "#59D200"
+    }
+    else {
+        resp2.innerText = ` ${nome} Você Não Foi Aprovado`
+        resp2.style.color = "#f40612"
+    }
+})
